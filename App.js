e0e665f713d69035e61220b65b40c0e133bddba7
natnael-OQP <makiya112545@gmail.com>
2022-03-28 22:11:51 +0300

    import ListingDetailsScreen

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
+import ListingDetailsScreen from './src/screens/ListingDetailsScreen'
 import WelcomeScreen from './src/screens/WelcomeScreen'
 
 export default function App() {
-    return <WelcomeScreen />
+    return <ListingDetailsScreen />
 }
