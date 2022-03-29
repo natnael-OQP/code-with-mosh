47024d05437d8bac812335a8c715714c90546e0b
natnael-OQP <makiya112545@gmail.com>
2022-03-30 00:25:58 +0300

    import ViewImageScreen

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 import ListingDetailsScreen from './src/screens/ListingDetailsScreen'
+import ViewImageScreen from './src/screens/ViewImageScreen'
 import WelcomeScreen from './src/screens/WelcomeScreen'
 
 export default function App() {
-    return <ListingDetailsScreen />
+    return <ViewImageScreen />
 }
