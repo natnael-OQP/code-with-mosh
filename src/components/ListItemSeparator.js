f39b214d4de040b8520d8b2be5c431ea6f879b60
natnael-OQP <makiya112545@gmail.com>
2022-03-31 15:41:21 +0300

    ListItemSeparator component created

---
+++ src/components/ListItemSeparator.js
@@ -0,0 +1,14 @@
+import { StyleSheet, View } from 'react-native'
+import colors from '../config/colors'
+
+export default function ListItemSeparator() {
+    return <View style={styles.container} />
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: 1,
+        backgroundColor: colors.light,
+    },
+})
