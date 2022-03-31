6a6706b670bb1311943d9c77e46c562ab3ad12a9
natnael-OQP <makiya112545@gmail.com>
2022-03-31 16:57:15 +0300

    import message screen

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 import ListingDetailsScreen from './src/screens/ListingDetailsScreen'
+import MessagesScreen from './src/screens/MessagesScreen'
+import MyAccountScreen from './src/screens/MyAccountScreen'
 import ViewImageScreen from './src/screens/ViewImageScreen'
 import WelcomeScreen from './src/screens/WelcomeScreen'
 
 export default function App() {
-    return <ViewImageScreen />
+    return <MessagesScreen />
 }
