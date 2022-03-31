f95464d826d36a8ac2d30368d8016d4ddb9ee2db
natnael-OQP <makiya112545@gmail.com>
2022-03-31 15:42:37 +0300

    add color

--- src/config/colors.js
+++ src/config/colors.js
@@ -1,7 +1,9 @@
 export default {
-  primary: "#fc5c65",
-  secondary: "#4ecdc4",
-  black: "#000",
-  white: "#fff",
-  medium: "#6e6969",
-};
+    primary: '#fc5c65',
+    secondary: '#4ecdc4',
+    black: '#000',
+    white: '#fff',
+    medium: '#6e6969',
+    light: '#F9F4F4',
+    danger: '#ff5252',
+}
