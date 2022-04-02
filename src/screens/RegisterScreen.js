39f12ff8388ba3beee65410e7e8a3ba79ccd2bce
natnael-OQP <makiya112545@gmail.com>
2022-04-02 09:50:03 +0300

    RegisterScreen created

---
+++ src/screens/RegisterScreen.js
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import { StyleSheet, Switch, View } from 'react-native'
+
+import AppTextInput from '../components/AppTextInput'
+import Screen from '../components/Screen'
+
+const RegisterScreen = () => {
+    const [value, setValue] = useState(false)
+    return (
+        <Screen style={styles.container}>
+            <AppTextInput name="email" size={20} placeholder="username" />
+            <Switch value={value} onValueChange={(value) => setValue(value)} />
+        </Screen>
+    )
+}
+
+export default RegisterScreen
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 6,
+    },
+})
