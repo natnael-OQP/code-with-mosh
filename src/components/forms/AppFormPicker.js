520a54f1b4a937cbe1eb360a76aa08bc6a8f328e
natnael-OQP <makiya112545@gmail.com>
2022-04-04 08:47:13 +0300

    pass appFormPicker

--- src/components/forms/AppFormPicker.js
+++ src/components/forms/AppFormPicker.js
@@ -5,7 +5,14 @@ import AppPicker from '../AppPicker'
 import AppText from '../AppText'
 import colors from '../../config/colors'
 
-function AppFormPicker({ items, name, width, placeholder }) {
+function AppFormPicker({
+    items,
+    name,
+    width,
+    placeholder,
+    numberOfColumns = 1,
+    PickerItemComponent,
+}) {
     const { errors, setFieldValue, touched, values } = useFormikContext()
 
     return (
@@ -13,6 +20,8 @@ function AppFormPicker({ items, name, width, placeholder }) {
             <AppPicker
                 width={width}
                 items={items}
+                numberOfColumns={numberOfColumns}
+                PickerItemComponent={PickerItemComponent}
                 onSelectItem={(item) => setFieldValue(name, item)}
                 placeholder={placeholder}
                 selectedItem={values[name]}
