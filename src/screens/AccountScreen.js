f305cadfef602e4269ac66d7d82e62f049e4b538
natnael-OQP <makiya112545@gmail.com>
2022-04-08 23:56:39 +0300

    AccountScreen created

---
+++ src/screens/AccountScreen.js
@@ -0,0 +1,69 @@
+import { StyleSheet, FlatList, View } from 'react-native'
+import React from 'react'
+import Icons from '../components/Icons'
+import Screen from '../components/screen'
+import ListItem from '../components/ListItem'
+import colors from '../config/colors'
+import ListItemSeparator from '../components/ListItemSeparator'
+
+const MenuItems = [
+    {
+        title: 'My Listings',
+        name: 'format-list-bulleted',
+        backgroundColor: colors.primary,
+    },
+    {
+        title: 'My Message',
+        name: 'email',
+        backgroundColor: colors.secondary,
+    },
+]
+
+const AccountScreen = () => {
+    return (
+        <Screen style={styles.screen}>
+            <ListItem
+                title="natnael"
+                subTitle="natnael-oqp"
+                image={require('../assets/mosh.jpg')}
+            />
+            <View style={styles.wrapper}>
+                <FlatList
+                    data={MenuItems}
+                    keyExtractor={(item) => item.name}
+                    renderItem={({
+                        item: { title, name, backgroundColor },
+                    }) => (
+                        <ListItem
+                            title={title}
+                            IconComponent={
+                                <Icons
+                                    name={name}
+                                    backgroundColor={backgroundColor}
+                                />
+                            }
+                        />
+                    )}
+                    ItemSeparatorComponent={ListItemSeparator}
+                />
+            </View>
+            <ListItem
+                title="logout"
+                IconComponent={
+                    <Icons name="logout" backgroundColor={colors.lightYellow} />
+                }
+            />
+        </Screen>
+    )
+}
+
+export default AccountScreen
+
+const styles = StyleSheet.create({
+    screen: {
+        backgroundColor: colors.light,
+    },
+    wrapper: {
+        marginVertical: 25,
+    },
+})
