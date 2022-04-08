7f032e4f976d21de83a6c5f6549b153097634003
natnael-OQP <makiya112545@gmail.com>
2022-04-08 08:21:16 +0300

    import AuthNavigator

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
-import ListingDetailsScreen from './src/screens/ListingDetailsScreen'
-import MessagesScreen from './src/screens/MessagesScreen'
-import MyAccountScreen from './src/screens/MyAccountScreen'
-import ViewImageScreen from './src/screens/ViewImageScreen'
-import WelcomeScreen from './src/screens/WelcomeScreen'
-import ListingScreen from './src/screens/ListingScreen'
-import RegisterScreen from './src/screens/RegisterScreen'
-import LoginScreen from './src/screens/LoginScreen'
-import ListingEditScreen from './src/screens/ListingEditScreen'
+// react navigation
+import { NavigationContainer } from '@react-navigation/native'
+
+import AuthNavigator from './src/navigation/AuthNavigator'
+import navigationThemes from './src/navigation/navigationThemes'
 
 export default function App() {
-    return <ListingEditScreen />
+    return (
+        <NavigationContainer theme={navigationThemes}>
+            <AuthNavigator />
+        </NavigationContainer>
+    )
 }
