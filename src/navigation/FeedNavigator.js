6ad7624562099f1603ccc8bafffad64611f4238f
natnael-OQP <makiya112545@gmail.com>
2022-04-10 12:19:22 +0300

    FeedNavigator completed

--- src/navigation/FeedNavigator.js
+++ src/navigation/FeedNavigator.js
@@ -7,16 +7,11 @@ import ListingDetailsScreen from '../screens/ListingDetailsScreen'
 export default function FeedNavigator() {
     const Stack = createNativeStackNavigator()
     return (
-        <Stack.Navigator mode="modal">
-            <Stack.Screen
-                name="Listing"
-                component={ListingScreen}
-                // options={{ headerShown: false }}
-            />
+        <Stack.Navigator mode="modal" screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Listing" component={ListingScreen} />
             <Stack.Screen
                 name="ListingDetails"
                 component={ListingDetailsScreen}
-                options={{ headerShown: false }}
             />
         </Stack.Navigator>
     )
