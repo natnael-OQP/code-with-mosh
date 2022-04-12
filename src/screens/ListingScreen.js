89669c54190297feb1eb15bc6a2c5f0f5baab986
natnael-OQP <makiya112545@gmail.com>
2022-04-12 15:00:24 +0300

    handle error

--- src/screens/ListingScreen.js
+++ src/screens/ListingScreen.js
@@ -1,23 +1,44 @@
-import { StyleSheet, FlatList } from 'react-native'
+import { StyleSheet, FlatList, ActivityIndicator } from 'react-native'
 import { useState, useEffect } from 'react'
 
 import Card from '../components/Card'
+import AppText from '../components/AppText'
+import AppButton from '../components/AppButton'
 import Screen from '../components/screen'
 import colors from '../config/colors'
 import instance from '../api/fetch'
 
 const ListingScreen = ({ navigation }) => {
     const [listings, setListings] = useState()
+    const [error, setError] = useState(false)
+    const [loading, setLoading] = useState(false)
 
-    useEffect(() => {
-        const fetcher = async () => {
+    const fetcher = async () => {
+        try {
+            setLoading(true)
             const { data } = await instance.get('listings')
             setListings(data)
+            setLoading(false)
+
+            setError(false)
+        } catch (err) {
+            setError(true)
         }
+    }
+
+    useEffect(() => {
         fetcher()
     }, [])
+
     return (
         <Screen style={styles.screen}>
+            {error && (
+                <>
+                    <AppText>Couldn't retrieve listings.</AppText>
+                    <AppButton title="retry" onPress={fetcher} />
+                </>
+            )}
+            {loading && <ActivityIndicator size="large" />}
             <FlatList
                 data={listings}
                 keyExtractor={(item) => item.id.toString()}
