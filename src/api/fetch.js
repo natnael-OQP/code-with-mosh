f96ec33ccff0c7ce259a1b32494863d250d2db37
natnael-OQP <makiya112545@gmail.com>
2022-04-13 17:50:02 +0300

    chnage ip address

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://192.168.1.12:9000/api/',
+    baseURL: 'http://192.168.1.7:9000/api/',
 })
 
 export default instance
